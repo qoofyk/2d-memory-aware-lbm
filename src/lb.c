3446fa38412621cc12cb04443bd18c396c633266
qoofyk <qoofyk@gmail.com>
2020-08-23 03:17:41 -0400

    add bridges compile patch; add bgk pragma ivdep

--- src/lb.c
+++ src/lb.c
@@ -201,8 +201,9 @@ void bgk(double* fPop, void* selfData) {
     double rho, ux, uy;
     computeMacros(fPop, &rho, &ux, &uy);
     double uSqr = ux*ux+uy*uy;
-    int iPop;
-    for(iPop=0; iPop<9; ++iPop) {
+    
+    #pragma ivdep
+    for(int iPop=0; iPop<9; ++iPop) {
         fPop[iPop] *= (1-omega);
         fPop[iPop] += omega * computeEquilibrium (
                                   iPop, rho, ux, uy, uSqr );
