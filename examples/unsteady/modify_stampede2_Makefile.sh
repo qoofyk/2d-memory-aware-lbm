3665381e8c567eb7149d24c393bfcbc0aa537781
qoofyk_stampede2 <qoofyk@gmail.com>
2020-08-23 02:05:32 -0500

    add stampede2 Makefile patch

---
+++ examples/unsteady/modify_stampede2_Makefile.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# First created: 
+# Last modified: 2020 Aug 18
+
+# Author: Yuankun Fu
+# email: qoofyk@gmail.com
+cd origin
+patch -p1 < ../stampede2.seq.Makefile.patch
+cd ../fuse
+patch -p1 < ../stampede2.seq.Makefile.patch
+cd ../fuse_tile
+patch -p1 < ../stampede2.seq.Makefile.patch
+cd ../2step
+patch -p1 < ../stampede2.seq.Makefile.patch
+cd ../2step_tile
+patch -p1 < ../stampede2.seq.Makefile.patch
+cd ../3step
+patch -p1 < ../stampede2.seq.Makefile.patch
+cd ../3step_tile
+patch -p1 < ../stampede2.seq.Makefile.patch
+
+cd ../origin_omp
+patch -p1 < ../stampede2.omp.Makefile.patch
+cd ../fuse_omp
+patch -p1 < ../stampede2.omp.Makefile.patch
+cd ../fuse_tile_omp
+patch -p1 < ../stampede2.omp.Makefile.patch
+cd ../2step_omp
+patch -p1 < ../stampede2.omp.Makefile.patch
+cd ../2step_tile_omp
+patch -p1 < ../stampede2.omp.Makefile.patch
+cd ../3step_omp
+patch -p1 < ../stampede2.omp.Makefile.patch
+cd ../3step_tile_omp
+patch -p1 < ../stampede2.omp.Makefile.patch
