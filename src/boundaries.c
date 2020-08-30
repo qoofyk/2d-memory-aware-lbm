be9b7fc92e699d0b0955741e49f23c791f4b6b1c
qoofyk <qoofyk@gmail.com>
2020-08-30 17:34:46 -0400

    dependency is not a problem at regularized splineq func()

--- src/boundaries.c
+++ src/boundaries.c
@@ -198,6 +198,7 @@ inline static void splitEqNeq (
         double* f, double* fEq, double* fNeq,
         double rho, double ux, double uy)
 {
+    #pragma ivdep
     for (int iPop=0; iPop<9; ++iPop) {
         fEq[iPop]
             = computeEquilibrium(iPop, rho, ux, uy, ux*ux+uy*uy);
@@ -209,6 +210,7 @@ inline static void regularizedF (
         double* f, double* fEq,
         double neqPixx, double neqPiyy, double neqPixy )
 {
+    #pragma ivdep
     for (int iPop=0; iPop<9; ++iPop) {
         f[iPop] = fEq[iPop] + 9./2. * t[iPop] *
             ( (c[iPop][0]*c[iPop][0]-1./3.)*neqPixx +
